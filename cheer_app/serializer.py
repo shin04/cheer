465e1079f4941c35fde337406da1c3b2c7a2134c
shin04 <daikon0413@gmail.com>
2019-10-05 22:36:46 +0900

    APIでPOST

--- cheer_app/serializer.py
+++ cheer_app/serializer.py
@@ -6,23 +6,28 @@ from .forms import SignUpForm
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'email')
-        extra_kwargs = { 'username': { 'read_only': False } }
+        fields = ('id', 'username', 'email')
+        #extra_kwargs = { 'username': { 'read_only': False } }
 
 class PostSerializer(serializers.ModelSerializer):
-    author = UserSerializer()
+    author = UserSerializer(read_only=True)
+    author_id = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), write_only=True)
     class Meta:
         model = Post
-        fields = ('author', 'title', 'text', 'created_date', 'published_date', 'pk')
+        fields = ('id', 'author', 'author_id', 'title', 'text', 'created_date', 'published_date')
 
-    def create(self, validated_data):
-        user_data = validated_data.pop('author')
-        user = User.objects.get(author=user_data)
-        post = Post.objects.create(author=user, **validated_data)
-        return post
+    def create(self, validated_date):
+        validated_date['author'] = validated_date.get('author_id', None)
+
+        if validated_date['author'] is None:
+            raise serializers.ValidationError("user not found.")
+
+        del validated_date['author_id']
+
+        return Post.objects.create(**validated_date)
 
 class CommentSerializer(serializers.ModelSerializer):
     post = PostSerializer()
     class Meta:
         model = Comment
-        fields = ('post', 'author', 'text', 'created_date')
+        fields = ('id', 'post', 'author', 'text', 'created_date')
